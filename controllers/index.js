6e347c7b07669af859f16663ed7b7a6d7c9ad5ab
Ankur Shahi <ankurshahi@MacBook-Air.phub.net.cable.rogers.com>
2021-07-25 20:38:07 -0400

    user model and CRUD routes added

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+// const homeRoutes = require('./home-routes.js');
+// const dashboardRoutes = require('./dashboard-routes.js');
+// collect the packaged group of API endpoints and prefix them with the `/api` path. 
+// router.use('/',homeRoutes);
+router.use('/api',apiRoutes);
+// router.use('/dashboard', dashboardRoutes);
+
+router.use((req,res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
