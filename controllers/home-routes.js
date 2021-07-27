7e007e93f9aaad6e38b4a8d1cc94d380e384a664
Ankur Shahi <ankurshahi@MacBook-Air.phub.net.cable.rogers.com>
2021-07-27 18:29:34 -0400

    single blog selection added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -40,6 +40,54 @@ router.get('/',(req,res)=>{
     });         
 });
 
+router.get('/blog/:id',(req,res)=>{
+    console.log('===============');
+    Blog.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes:[
+            'id',
+            'blog_text',
+            'title',
+            'created_at',
+            // [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id','comment_text','blog_id','user_id','created_at'],
+                include: {
+                    model:User,
+                    attribute: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes:['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+
+        if(!dbPostData) {
+            res.status(404).json({message:'No blog found with this id'});
+            return;
+        }
+
+        const blog = dbPostData.get({plain:true});
+
+        res.render('single-blog', {
+            blog,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });         
+});
+
 router.get('/login',(req,res)=>{
     if(req.session.loggedIn) {
         res.redirect('/');
