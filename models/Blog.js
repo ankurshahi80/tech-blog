95fa3315486ff60ab83d648c11679fbe273b05e1
Ankur Shahi <ankurshahi@MacBook-Air.local>
2021-08-02 01:33:41 -0400

    code cleaned and sign up bug corrected

--- models/Blog.js
+++ models/Blog.js
@@ -2,39 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Blog model
-class Blog extends Model{
-    // static upvote(body, models){
-    //     return models.Vote.create({
-    //         user_id:body.user_id,
-    //         post_id:body.post_id
-    //     }).then(() => {
-    //         // find the post we just voted on 
-    //         return Post.findOne({
-    //             where: {
-    //                 id:body.post_id
-    //             },
-    //             attributes:[
-    //                 'id',
-    //                 'post_url',
-    //                 'title',
-    //                 'created_at',
-    //                 // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name 'vote_count'
-    //                 [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id=vote.post_id)'),'vote_count']
-    //             ],
-    //             include:[
-    //                 {
-    //                     model:models.Comment,
-    //                     attributes: ['id','comment_text','post_id','user_id','created_at'],
-    //                     include: {
-    //                         model: models.User,
-    //                         attributes: ['username']
-    //                     }
-    //                 }
-    //             ]
-    //         });
-    //     });
-    // }
-};
+class Blog extends Model{};
 
 // create fields/columns for Blog model
 Blog.init(
