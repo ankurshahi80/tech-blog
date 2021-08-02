09846687fc62fd61b4d101bbd018dae64b171bbe
Ankur Shahi <ankurshahi@MacBook-Air.local>
2021-08-02 00:30:53 -0400

    update post added

--- public/javascript/updateBlog.js
+++ public/javascript/updateBlog.js
@@ -1,25 +1,27 @@
 async function updateBlog(event){
     event.preventDefault();
-    alert("listening to update");
-    // const title = document.getElementById("add-title").value.trim();
-    // const content = document.getElementById('addContent').value.trim();
-    // console.log("listening", title, content);
-    // const response = await fetch('/api/blogs',{
-    //     method: 'POST',
-    //     body: JSON.stringify({
-    //         title: title,
-    //         blog_text: content
-    //     }),
-    //     headers: {
-    //         'Content-Type':'application/json'
-    //     }
-    // });
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
+    const title = document.getElementById("update-title").value.trim();
+    const content = document.getElementById('update-content').value.trim();
+    console.log("listening", id, title, content);
+    const response = await fetch(`/api/blogs/${id}`,{
+        method: 'PUT',
+        body: JSON.stringify({
+            title: title,
+            blog_text: content
+        }),
+        headers: {
+            'Content-Type':'application/json'
+        }
+    });
 
-    // if(response.ok){
-    //     document.location.replace('/dashboard');
-    // } else {
-    //     alert(response.status.text);
-    // }
+    if(response.ok){
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.status.text);
+    }
 };
 
 document.getElementById('update-btn').addEventListener('click', updateBlog);
